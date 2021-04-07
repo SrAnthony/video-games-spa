58e658dca5423fd0ab270abe1cdf1f6e5f50210d
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2021-04-06 21:15:03 -0300

    :sparkles: Adiciona cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Contact from './Pages/Contact/Contact'
 import VideoGames from './Pages/VideoGames/VideoGames'
+import styled from 'styled-components'
+import Header from './Components/Header'
+import './global.css'
 
 const App = () => {
   
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<VideoGames />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+    <Container>
+      <Content>
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<VideoGames />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Content>
+    </Container>
   )
 }
 
 export default App
+
+const Container = styled.div`
+  min-height: 100vh;
+`
+
+const Content = styled.div`
+  max-width: 1500px;
+  margin: auto;
+`
---
+++ src/Components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header: React.FC = () => {
+  
+  return (
+    <Container>
+      <MenuItem>
+        VIDEO GAMES
+      </MenuItem>
+      
+      <MenuItem>
+        CONTACT
+      </MenuItem>
+    </Container>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+  color: white;
+  margin-top: 100px;
+`
+
+const MenuItem = styled.button`
+  font-size: 42px;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  color: white;
+  margin-left: 100px;
+`
